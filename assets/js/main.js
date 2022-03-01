6afbb13957891fb5bb40517de5322602a9c6f30d
aliniko <aliverchan@gmail.com>
2022-03-01 23:13:02 +0100

    background header

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,15 @@
 /*==================== MENU SHOW Y HIDDEN ====================*/
 
 
+/*==================== CHANGE BACKGROUND HEADER ====================*/ 
+function scrollHeader(){
+    const header = document.getElementById('header')
+    // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+    if(this.scrollY >= 50) header.classList.add('scroll-header'); 
+    else header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
+
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
 
@@ -29,8 +38,6 @@
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 
-
-/*==================== CHANGE BACKGROUND HEADER ====================*/ 
 
 
 /*==================== SHOW SCROLL UP ====================*/ 
