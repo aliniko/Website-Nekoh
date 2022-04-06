f7969b350acfcbd1c72dea74c21f73758ae3ce94
aliniko <aliverchan@gmail.com>
2022-04-06 23:56:41 +0200

    scrolbar

--- assets/js/main.js
+++ assets/js/main.js
@@ -81,6 +81,28 @@ let  swiperTestimonial = new Swiper(".testimonial_container", {
 /* Validate if constant exists */
 
 
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight,
+              sectionTop = current.offsetTop - 58,
+              sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+
 /*==================== REMOVE MENU MOBILE ====================*/
 
 
@@ -97,9 +119,6 @@ let  swiperTestimonial = new Swiper(".testimonial_container", {
 
 
 /*==================== TESTIMONIAL ====================*/
-
-
-/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 
 
 
